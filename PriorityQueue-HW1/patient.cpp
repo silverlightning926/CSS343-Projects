56627adbb4a9f0e9456b4d88fd9d34202cdd7307
Siddharth <siddrao.926@gmail.com>
2022-10-07 08:14:49 -0700

    Documented Patient Class

--- PriorityQueue-HW1/patient.cpp
+++ PriorityQueue-HW1/patient.cpp
@@ -1,48 +1,69 @@
-//
-// Created by siddr on 10/5/2022.
-//
+/*
+ * patient.cpp
+ *
+ *  Cpp File For Patient Class
+ */
 
 #include "patient.h"
 #include <iostream>
 
+// Implementation Of Patient Class Header
 Patient::Patient(std::string name, int age, bool hasPrecondition) {
   this->name = std::move(name);
   this->age = age;
   this->hasPrecondition = hasPrecondition;
 }
 
-std::string Patient::getName() const { return name; }
+std::string Patient::getName() const {
+  return name;
+} // Implementation Of name Field Getter
 
-void Patient::setName(std::string newValue) { name = std::move(newValue); }
+void Patient::setName(std::string newValue) {
+  name = std::move(newValue);
+} // Implementation Of name Field Setter
 
-int Patient::getAge() const { return age; }
+int Patient::getAge() const {
+  return age;
+} // Implementation Of age Field Getter
 
-void Patient::setAge(int newValue) { age = std::move(newValue); }
+void Patient::setAge(int newValue) {
+  age = std::move(newValue);
+} // Implementation of age Field Setter
 
-bool Patient::getHasPrecondition() const { return hasPrecondition; }
+bool Patient::getHasPrecondition() const {
+  return hasPrecondition;
+} // Implementation of hasPrecondition Getter
 
-void Patient::setHasPrecondition(bool newValue) { hasPrecondition = newValue; }
+void Patient::setHasPrecondition(bool newValue) {
+  hasPrecondition = newValue;
+} // Implementation of hasPrecondition Setter
 
+// Implementation Of getPriority Function
 int Patient::getPriorityValue() const {
   return getAge() * (getHasPrecondition() ? 4 : 1);
 }
 
+// Implementation of Operator < Overload
 bool Patient::operator<(const Patient &patient) const {
   return getPriorityValue() < patient.getPriorityValue();
 }
 
+// Implementation of Operator <= Overload
 bool Patient::operator<=(const Patient &patient) const {
   return getPriorityValue() <= patient.getPriorityValue();
 }
 
+// Implementation of Operator > Overload
 bool Patient::operator>(const Patient &patient) const {
   return getPriorityValue() > patient.getPriorityValue();
 }
 
+// Implementation of Operator >= Overload
 bool Patient::operator>=(const Patient &patient) const {
   return getPriorityValue() >= patient.getPriorityValue();
 }
 
+// Implementation of Operator << Overload
 std::ostream &operator<<(std::ostream &out, const Patient &patient) {
   out << "[ Name: ";
   out << patient.getName();
--- PriorityQueue-HW1/patient.h
+++ PriorityQueue-HW1/patient.h
@@ -1,6 +1,8 @@
 /*
-* patient.h
-*/
+ * patient.h
+ *
+ * Header File For Patient Class
+ */
 
 #ifndef COVIDPRIORITYQUEUE_P1_PATIENT_H
 #define COVIDPRIORITYQUEUE_P1_PATIENT_H
@@ -14,27 +16,32 @@ private:
   bool hasPrecondition;
 
 public:
-  Patient(std::string name, int age, bool hasPrecondition);
-  Patient() = delete;
+  Patient(std::string name, int age,
+          bool hasPrecondition); // Constructor For Parent Object
+  Patient() = delete;            // Delete Basic Constructor
 
-  friend std::ostream &operator<<(std::ostream &out, const Patient &patient);
+  friend std::ostream &
+  operator<<(std::ostream &out,
+             const Patient &patient); // Overloading Print Operator
 
-  std::string getName() const;
-  void setName(std::string newValue);
+  std::string getName() const;        // Getter Function dor name Field
+  void setName(std::string newValue); // Setter Function For name Field
 
-  int getAge() const;
-  void setAge(int newValue);
+  int getAge() const;        // Getter Function For age Field
+  void setAge(int newValue); // Setter Function For age Field
 
-  bool getHasPrecondition() const;
-  void setHasPrecondition(bool newValue);
+  bool getHasPrecondition() const; // Getter Function For hasPrecondition Field
+  void
+  setHasPrecondition(bool newValue); // Setter Function For hasPreconditionField
 
-  int getPriorityValue() const;
+  int getPriorityValue()
+      const; // Function To Get Priority Value of the Patient Object
 
-  bool operator<(const Patient &patient) const;
-  bool operator<=(const Patient &patient) const;
+  bool operator<(const Patient &patient) const;  // Overloading < Operator
+  bool operator<=(const Patient &patient) const; // Overloading <= Operator
 
-  bool operator>(const Patient &patient) const;
-  bool operator>=(const Patient &patient) const;
+  bool operator>(const Patient &patient) const;  // Overloading > Operator
+  bool operator>=(const Patient &patient) const; // Overloading >= Operator
 };
 
 #endif // COVIDPRIORITYQUEUE_P1_PATIENT_H
