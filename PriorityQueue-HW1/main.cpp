a27898650a671ed04d1abb8abb7c72388d8bd779
Siddharth <siddrao.926@gmail.com>
2022-10-07 07:38:03 -0700

    Added Documentation To main.cpp

--- PriorityQueue-HW1/main.cpp
+++ PriorityQueue-HW1/main.cpp
@@ -1,3 +1,16 @@
+/*
+* main.cpp
+*
+* This is the main driver class for Covid Priority Queue - HW1
+*
+* It handles reading from a file and parsing the data and creating
+    patient objects from that data. It then adds that data to the 
+    CovidPriorityQueue object. The class then asks the user for how
+    many doses are available, and looks at and prints the top of the 
+    queue and pops the queue accordingly. It will print as many patients 
+    as the user requested unless the queue becomes empty. 
+*/
+
 #include <fstream>
 #include <iostream>
 #include <limits>
@@ -8,10 +21,12 @@
 #include "Patient.h"
 
 int main() {
-  std::vector<std::vector<std::string>> data;
-  std::ifstream infile("./patients.txt");
 
-  while (infile) {
+  std::ifstream infile("./patients.txt");   // Accessing The File
+
+  std::vector<std::vector<std::string>> data; 
+
+  while (infile) {  // Reading The File
     std::string s;
     if (!getline(infile, s))
       break;
@@ -20,7 +35,7 @@ int main() {
     std::vector<std::string> record;
 
     while (ss) {
-      if (!getline(ss, s, ','))
+      if (!getline(ss, s, ',')) // Parsing The File
         break;
       record.push_back(s);
     }
@@ -28,12 +43,12 @@ int main() {
     data.push_back(record);
   }
   if (!infile.eof()) {
-    std::cerr << "Error Finding File!\n";
+    std::cerr << "Error Finding File!\n";   // If File Not Found
   }
 
-  CovidPriorityQueue covid_pq;
+  CovidPriorityQueue covid_pq;  // Creating CovidPriorityQueue Object
 
-  for (int i = 1; i < 5; ++i) {
+  for (int i = 1; i < 5; ++i) { // Creating Patient Objects From Parsed Data & Adding Them To The Queue
     std::string name;
     int age;
     bool has_precondition;
@@ -57,9 +72,9 @@ int main() {
 
   int vaccine_doses;
 
-  std::cout << "Enter Available Doses: ";
+  std::cout << "Enter Available Doses: ";   // Querying The User For The Amount Of Available Doses
 
-  while (!(std::cin >> vaccine_doses)) {
+  while (!(std::cin >> vaccine_doses)) {    // Requery The User If Input Is Not A Number
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     std::cout << "Invalid input. Try again: ";
@@ -67,7 +82,7 @@ int main() {
 
   std::cout << std::endl;
 
-  for (int i = 0; i < vaccine_doses && !covid_pq.empty(); ++i) {
+  for (int i = 0; i < vaccine_doses && !covid_pq.empty(); ++i) {    // Print Out Top Priority Patients
     std::cout << i + 1 << ". " << covid_pq.top() << std::endl;
     covid_pq.pop();
   }
